9da7522d3e304c2746cac481048d3026fca5cb86
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2024-08-27 11:19:11 +0700

    route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import { Route, Routes, } from 'react-router-dom'
 import { HomePage, LoginPage, SignupPage } from './pages'
-
+import Main from './layout/Main'
 
 function App() {
-
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Main />} >
+          <Route path="/" element={<HomePage />} />
+        </Route>
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+
       </Routes>
     </>
   )
